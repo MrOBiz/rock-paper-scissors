578cfcca82cba0924b59522131840252491a979d
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-06-30 08:54:13 +0200

    Added getHumanChoice function and differentiated computer inputs

--- myscript.js
+++ myscript.js
@@ -1,13 +1,34 @@
+let humanInput = +prompt("What's your choice? 1: Rock, 2: Paper, 3: Scissors", " ");
+
 function getComputerChoice(){
     let choice = (Math.random() * 100);
 
     if (choice >= 0 && choice < 33){
-        console.log("Rock");
+        console.log("Computer: Rock");
     }else if (choice >= 33 && choice < 66){
-        console.log("paper");
+        console.log("Computer: Paper");
     }else{
-        console.log("Scissors");
+        console.log("Computer: Scissors");
     }
 }
 
 getComputerChoice();
+
+function getHumanChoice(choice){
+    
+    switch (choice){
+        case 1:
+            console.log("Rock");
+            break;
+
+        case 2:
+            console.log("Paper");
+            break;
+
+        case 3:
+            console.log("Scissors");
+            break;
+    }
+}
+
+getHumanChoice(humanInput);
