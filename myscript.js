50f18353ebfeaecdeee6e7c0bab98d3287dd82b9
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-06-30 12:35:31 +0200

    Refactored playGame function and found a bug.
    Tried to make it so that after each round a console.log()
    line would be printed to explicitly say the 2 choices,
    but they got mixed up.
    
    Solving this bug next.

--- myscript.js
+++ myscript.js
@@ -1,10 +1,5 @@
-let humanScore = 0;
-let computerScore = 0;
-
+let humanChoice;
 let computerChoice = getComputerChoice();
-let humanChoice = prompt("What's your choice? (Rock, Paper, Scissors)", " ");
-
-humanChoice = humanChoice.toLowerCase();
 
 function getComputerChoice(){
     let choice = (Math.random() * 100);
@@ -40,38 +35,60 @@ function getHumanChoice(choice){
     }
 }
 
-getHumanChoice(humanChoice);
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    let i = 0;
 
-function playRound(hChoice, cChoice){
+    function playRound(hChoice, cChoice){
+
+        computerChoice = getComputerChoice();
+        humanChoice = getHumanChoice(humanChoice);
     
-    if(hChoice == "rock" && cChoice == "rock"){
-        console.log("Draw!");
-    }else if(hChoice == "paper" && cChoice == "paper"){
-        console.log("Draw!");
-    }else if(hChoice == "scissors" && cChoice == "scissors"){
-        console.log("Draw!");
-    }else if(hChoice == "rock" && cChoice == "scissors"){
-        console.log("You win!");
-        humanScore +=1;
-    }else if(hChoice == "paper" && cChoice == "rock"){
-        console.log("You win!");
-        humanScore +=1;
-    }else if(hChoice == "scissors" && cChoice == "paper"){
-        console.log("You win!");
-        humanScore +=1;
-    }else if(hChoice == "scissors" && cChoice == "rock"){
-        console.log("Computer wins!");
-        computerScore +=1;
-    }else if(hChoice == "rock" && cChoice == "paper"){
-        console.log("Computer wins!");
-        computerScore +=1;
-    }else if(hChoice == "paper" && cChoice == "scissors"){
-        console.log("Computer wins!");
-        computerScore +=1;
+        if(hChoice == "rock" && cChoice == "rock"){
+            console.log("Draw!");
+        }else if(hChoice == "paper" && cChoice == "paper"){
+            console.log("Draw!");
+        }else if(hChoice == "scissors" && cChoice == "scissors"){
+            console.log("Draw!");
+        }else if(hChoice == "rock" && cChoice == "scissors"){
+            console.log("You win!");
+            humanScore +=1;
+        }else if(hChoice == "paper" && cChoice == "rock"){
+            console.log("You win!");
+            humanScore +=1;
+        }else if(hChoice == "scissors" && cChoice == "paper"){
+            console.log("You win!");
+            humanScore +=1;
+        }else if(hChoice == "scissors" && cChoice == "rock"){
+            console.log("Computer wins!");
+            computerScore +=1;
+        }else if(hChoice == "rock" && cChoice == "paper"){
+            console.log("Computer wins!");
+            computerScore +=1;
+        }else if(hChoice == "paper" && cChoice == "scissors"){
+            console.log("Computer wins!");
+            computerScore +=1;
+        }
+
+        console.log(computerScore);
+        console.log(humanScore);
     }
 
-    console.log(computerScore);
-    console.log(humanScore);
+
+    while(i<5){
+        humanChoice = prompt("What's your choice? (Rock, Paper, Scissors)", " ");
+        humanChoice = humanChoice.toLowerCase();
+
+        playRound(humanChoice, computerChoice);
+        i++;
+    }
+
+    if(computerScore > humanScore){
+        console.log("Computer won the game!")
+    }else if(computerScore < humanScore){
+        console.log("You won the game!")
+    }
 }
 
-playRound(humanChoice, computerChoice);
+playGame();
