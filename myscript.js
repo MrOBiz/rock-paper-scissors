3347c1fe225b5e6f18d0462d57936c78ee3e424b
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-06-30 09:03:33 +0200

    Added variables for continuous play, refactored some code

--- myscript.js
+++ myscript.js
@@ -1,18 +1,27 @@
-let humanInput = +prompt("What's your choice? 1: Rock, 2: Paper, 3: Scissors", " ");
+const humanScore = 0;
+const computerScore = 0;
+
+const computerChoice = getComputerChoice();
+const humanChoice = +prompt("What's your choice? 1: Rock, 2: Paper, 3: Scissors", " ");
+
+
 
 function getComputerChoice(){
     let choice = (Math.random() * 100);
 
     if (choice >= 0 && choice < 33){
         console.log("Computer: Rock");
+        choice = 1;
     }else if (choice >= 33 && choice < 66){
         console.log("Computer: Paper");
+        choice = 2;
     }else{
         console.log("Computer: Scissors");
+        choice = 3;
     }
-}
 
-getComputerChoice();
+    return choice;
+}
 
 function getHumanChoice(choice){
     
@@ -31,4 +40,4 @@ function getHumanChoice(choice){
     }
 }
 
-getHumanChoice(humanInput);
+getHumanChoice(humanChoice);
